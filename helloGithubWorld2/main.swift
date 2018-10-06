7169c1e3a4b8632c15b217d3fa566fe7db968f99
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2018-10-06 17:37:44 -0400

    first commit

---
+++ helloGithubWorld2/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  helloGithubWorld2
+//
+//  Created by Elizabeth Peraza  on 10/6/18.
+//  Copyright © 2018 Elizabeth Peraza . All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+print(2+2)
+
+
